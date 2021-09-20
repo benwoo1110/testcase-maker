5174d40d12a1c51d451652cacaa8728f270aa029
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-09-20 22:17:57 +0800

    Fix import issues.

--- testcase_maker/values/__init__.py
+++ testcase_maker/values/__init__.py
@@ -1,10 +1,10 @@
-from base import BaseValue, ValueContainer
-from constant import Constant
-from loop_value import LoopValue
-from named_value import NamedValue
-from random_int import RandomInt
-from random_item import RandomItem
-from random_sample import RandomSample
-from range import Range
-from value_ref import ValueRef
-from value_group import ValueGroup
+from .base import BaseValue, ValueContainer
+from .constant import Constant
+from .loop_value import LoopValue
+from .named_value import NamedValue
+from .random_int import RandomInt
+from .random_item import RandomItem
+from .random_sample import RandomSample
+from .range import Range
+from .value_ref import ValueRef
+from .value_group import ValueGroup
