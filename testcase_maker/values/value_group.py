d7029749948b062b1971a289f0fab858912bb23d
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-09-21 11:07:51 +0800

    Remove useless mappings.

--- testcase_maker/values/value_group.py
+++ testcase_maker/values/value_group.py
@@ -1,8 +1,8 @@
-from typing import List, Any, Union, TYPE_CHECKING, Dict
+from typing import List, Union, TYPE_CHECKING
 
 import attr
 
-from testcase_maker.values import BaseValue, ValueContainer, NamedValue, Constant, LoopValue
+from testcase_maker.values import BaseValue, Constant, LoopValue
 
 if TYPE_CHECKING:
     from testcase_maker.resolver import Resolver
@@ -11,21 +11,9 @@ if TYPE_CHECKING:
 @attr.define()
 class ValueGroup(BaseValue):
     values: List[BaseValue] = attr.ib(factory=list)
-    value_name_map: Dict[BaseValue, Any] = attr.ib(factory=dict)
-    name_value_map: Dict[Any, BaseValue] = attr.ib(factory=dict)
 
     def add(self, value: BaseValue):
         self.values.append(value)
-
-        while isinstance(value, ValueContainer):
-            parent, value = value, value.value
-            if isinstance(parent, NamedValue):
-                # self.value_name_map[value] = parent.name
-                self.name_value_map[parent.name] = value
-
-        if isinstance(value, ValueGroup):
-            # self.value_name_map.update(value.value_name_map)
-            self.name_value_map.update(value.name_value_map)
 
     def space(self):
         self.add(Constant(" "))
