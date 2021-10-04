40326b4bd5acb51f67a17b3d4aac74981a5ee14a
Daksh Kanotra <59220834+daksh10110@users.noreply.github.com>
2021-10-04 22:14:01 +0530

    Update testcase_maker/executors/cpp.py
    Co-authored-by: Ben Woo <30431861+benwoo1110@users.noreply.github.com>

--- testcase_maker/executors/cpp.py
+++ testcase_maker/executors/cpp.py
@@ -17,7 +17,7 @@ class CPPExecutor(Executor):
         return "cpp"
 
     def compile(self, tempdir: Union["Path", str], source_filename: Union["Path", str]) -> Union["Path", str]:
-        args = ["g++", "-o", str(source_filename.stem), str(source_filename)]
+        args = ["g++", "-o", str(source_filename.stem), str(source_filename.absolute())]
         run_command(args, cwd=tempdir)
         return tempdir.joinpath(f"{source_filename.stem}.exe")
 
