45c1934e2c6ed34f5d28d68f72be1f4e62d1b6bf
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-10-10 23:23:18 +0800

    Make encoding type a parameter.

--- testcase_maker/utils.py
+++ testcase_maker/utils.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 from subprocess import Popen, PIPE
-from typing import List
+from typing import List, Optional
 
 
-def run_command(args: List[str], stdin: str = None, cwd: "Path" = None):
+def run_command(args: List[str], stdin: Optional[str] = None, cwd: Optional["Path"] = None, encode_type: str = "UTF-8"):
     if stdin:
         stdin = stdin.encode()
 
     process = Popen(args, stdout=PIPE, stdin=PIPE, stderr=PIPE, cwd=cwd)
     out = process.communicate(stdin)
     if out[1]:
-        print(out[1].decode("UTF-8"))
+        print(out[1].decode(encode_type))
         raise Exception("Error executing answer script.")
 
-    return out[0].decode("utf-8")
+    return out[0].decode(encode_type)
